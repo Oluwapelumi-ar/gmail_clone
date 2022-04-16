508217e71927e915f98254f852833cb8d1b98acd
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-16 12:58:20 +0100

    styled compose mail button

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,4 +1,5 @@
-import { AddCircleOutline } from '@mui/icons-material';
+
+import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import React from 'react';
 import './SideBar.css'
@@ -6,9 +7,9 @@ import './SideBar.css'
 const SideBar = () => {
   return (
     <div className='sidebar'>
-     <Button startIcon={AddCircleOutline}> Compose</Button>
+     <Button startIcon={<AddIcon fontSize='large' />} className="sidebar__compose"> Compose</Button>
     </div>
   )
 }
 
-export default SideBar
+export default SideBar;
