d6fba5e9142f7901cc891ef327a5b70c9e8f1eb9
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-16 22:07:38 +0100

    Sidebar Initial Setup

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,13 +1,28 @@
 
+import { Inbox,Star, AccessTime, LabelImportant, NearMe, Note, ExpandMore } from '@mui/icons-material';
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import React from 'react';
-import './SideBar.css'
+import './SideBar.css';
+import SideBarOption from './SideBarOption';
+
+
 
 const SideBar = () => {
   return (
     <div className='sidebar'>
-     <Button startIcon={<AddIcon fontSize='large' />} className="sidebar__compose"> Compose</Button>
+     <Button 
+        startIcon={<AddIcon fontSize='large' />} className="sidebar__compose"
+      > 
+        Compose
+      </Button>
+      <SideBarOption Icon={Inbox} title={'Inbox'} number={4} selected={true}/>
+      <SideBarOption Icon={Star} title={'Starred'} number={10}/>
+      <SideBarOption Icon={AccessTime} title={'Snoozed'} number={10}/>
+      <SideBarOption Icon={LabelImportant} title={'Important'} number={10}/>
+      <SideBarOption Icon={NearMe} title={'Sent'} number={10}/>
+      <SideBarOption Icon={Note} title={'Draft'} number={20}/>
+      <SideBarOption Icon={ExpandMore} title={'More'} number={10}/>
     </div>
   )
 }
