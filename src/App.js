eb02d3babbcdf4b33e9f27fe3fc955a7ba53b020
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-17 18:25:15 +0100

    bbuilt the login page and persisted log in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import SideBar from './SideBar';
@@ -6,31 +6,53 @@ import Mail from './Mail';
 import EmailList from './component/EmailList';
 import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
 import SendMail from './component/SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { login, selectUser } from './features/userSlice';
+import Login from './component/Login';
+import { auth } from './firebase';
 
 function App() {
-  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        dispatch(login({
+          displayName:user.displayName,
+          email:user.email,
+          photoUrl:user.photoURL
+        }))
+      }
+    })
+  },[])
   return (
     <Router>
+      {!user ?(
+        <Login />
+      ):(
       <div className="App">
-      <Header />
+        <Header />
 
-      <div className="app__body">
-        <SideBar />
+        <div className="app__body">
+          <SideBar />
 
-        <Switch>
-          <Route path='/mail' >
-            <Mail />
-          </Route>
-          <Route path='/'>
-            <EmailList />
-          </Route>
-        </Switch>
-      </div>
-      
-      {sendMessageIsOpen&& <SendMail />}
+          <Switch>
+            <Route path='/mail' >
+              <Mail />
+            </Route>
+            <Route path='/'>
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+        
+        {sendMessageIsOpen&& <SendMail />}
     </div>
+      )}
+      
     </Router>
     
   );
