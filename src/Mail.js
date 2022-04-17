a464a5bff73b89a984b84761f5e4fe6a03bfba3d
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-17 17:13:26 +0100

    fetched data from firebase and sending data

--- src/Mail.js
+++ src/Mail.js
@@ -1,12 +1,15 @@
 import { ArrowBack, CheckCircle, Delete, Email, Error, ExitToApp, LabelImportant, MoreVert, MoveToInbox, Print, UnfoldMore, WatchLater } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { selectOpenMail } from './features/mailSlice';
 import  './Mail.css';
 
 function Mail() {
 
   const history = useHistory()
+  const selectedMail = useSelector(selectOpenMail)
   return (
     <div className='mail'>
       <div className="mail__tools">
@@ -56,14 +59,14 @@ function Mail() {
       </div>
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportant className='mail__important'/>
-          <p>Title</p>
-          <p className='mail__time'>10pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className='mail__time'>{selectedMail?.time}</p>
         </div>
 
         <div className="mail__message">
-          <p>This is a message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
--- src/features/mailSlice.js
+++ src/features/mailSlice.js
@@ -4,9 +4,13 @@ import {  createSlice } from '@reduxjs/toolkit';
 export const mailSlice = createSlice({
   name: 'mail',
   initialState:{
+    selectedMail:null,
     sendMessageIsOpened:false
   },
   reducers: {
+    selectMail:(state,action) => {
+      state.selectedMail = action.payload
+    },
     openSendMessage: (state) => {
       
       state.sendMessageIsOpened = true;
@@ -19,10 +23,11 @@ export const mailSlice = createSlice({
   
 });
 
-export const { openSendMessage, closeSendMessage } = mailSlice.actions;
+export const { selectMail,openSendMessage, closeSendMessage } = mailSlice.actions;
 
 
 export const selectSendMessageIsOpen = (state) => state.mail.sendMessageIsOpened;
+export const selectOpenMail = (state) => state.mail.selectedMail;
 
 
 export default mailSlice.reducer;
